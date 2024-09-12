e0e9a575dbb4ce9aeecfe9959974589eb6684868
n.tilavov <n.tilavov@newuu.uz>
2024-09-12 15:16:31 +0500

    init

--- problem1.cpp
+++ problem1.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int a,b,c;
-    cin>>a>>b>>c;
-    cout<<a-b-c<<endl;
-    cout<<b*c-a<<endl;
-    cout<<2*b-c+5*a<<endl;
 
-    return 0;
+int main() {
+    int a=24;
+    float b=3.14;
+    double doubleNumber=45.1234;
+    bool boolean=true;
+    char charName='A';
+    cout<<"The value of a is "<<a<<" Size is "<<sizeof(a)<<endl;
+    cout<<"The value of a is "<<b<<" Size is "<<sizeof(b)<<endl;
+    cout<<"The value of a is "<<doubleNumber<<" Size is "<<sizeof(doubleNumber)<<endl;
+    cout<<"The value of a is "<<boolean<<" Size is "<<sizeof(boolean)<<endl;
+    cout<<"The value of a is "<<charName<<" Size is "<<sizeof(charName)<<endl;
+
 }
+//1-misol
+
+
